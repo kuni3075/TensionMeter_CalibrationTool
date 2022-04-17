eba7d611019fb198d69c8b4fd4f34447f718cb64
kuni3075 <kuni_3075@yahoo.co.jp>
2022-04-17 22:19:23 +0900

    ver1.0

--- CalibrationTool.py
+++ CalibrationTool.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+import time
 from turtle import shape
 import serial
 from serial.tools import list_ports
@@ -9,6 +10,7 @@ import numpy as np
 # 変数定義
 measure_flag = False
 send_flag = False
+zero_set_flag = False
 comport = ""
 weight = 0
 value = 0
@@ -54,10 +56,19 @@ def button4_click():
 def button5_click():
     with open('Proofread.txt','r') as f:
         entry2_text.set(f.readline())
+        
+def button6_click():
+    global zero_set_flag
+    
+    button6_text.set("調整中")
+    button6.state(['pressed'])
+    button6.state(['disabled'])
+    zero_set_flag = True
     
 def SubLoop():
     global measure_flag
     global send_flag
+    global zero_set_flag
     global comport
     
     while True:
@@ -83,6 +94,11 @@ def SubLoop():
             button2.state(['!disabled'])
             button2.state(['!pressed'])
             button2_text.set("送信")
+        elif zero_set_flag:
+            ZeroSetProcess()
+            zero_set_flag = False
+            button6.state(['!disabled'])
+            button6.state(['!pressed'])
         else:
             label6_text.set("")
 
@@ -128,6 +144,21 @@ def SendProcess():
     else:
         label5_text.set("通信エラー")
     ser.close()
+    
+def ZeroSetProcess():
+    var_str = ""
+    ser = serial.Serial(comport, 115200, timeout=0.5)
+    ser.reset_input_buffer()
+    ser.write(str.encode("0\r\n"))
+    
+    if ser.readline().decode() == "Base Update\r\n":
+        while var_str != "Finished":
+            var_str = ser.readline().decode().replace("\r\n", "")
+            button6_text.set(var_str)
+        button6_text.set("開始")
+    else:
+        button6_text.set("通信エラー")
+    
 
 # ===============================================================
 # フレームの作成
@@ -138,38 +169,53 @@ frame1 = ttk.Frame(relief='raised')
 frame2 = ttk.Frame(relief='raised')
 frame3 = ttk.Frame(relief='raised')
 frame4 = ttk.Frame(relief='raised')
+frame5 = ttk.Frame(relief='raised')
 
 # フレームの設定
 frame1.grid(row=0, column=0, padx=5, pady=5, ipadx=5, ipady=5)
 frame2.grid(row=1, column=0, padx=5, pady=5, ipadx=5, ipady=5)
 frame3.grid(row=2, column=0, padx=5, pady=5, ipadx=5, ipady=5)
 frame4.grid(row=3, column=0, padx=5, pady=5, ipadx=5, ipady=5)
+frame5.grid(row=4, column=0, padx=5, pady=5, ipadx=5, ipady=5)
 
 # ===============================================================
 # ビューの作成
-
-# frame1
-label1 = ttk.Label(frame1, text="接続先")
-combobox1 = ttk.Combobox(frame1, width=40, state="readonly", values=[])
-
-
-# frame2
-button1_text = tk.StringVar(frame2)
-button1_text.set("測定開始")
-label6_text = tk.StringVar(frame2)
+label5_text = tk.StringVar(frame5)
+label5_text.set("")
+label6_text = tk.StringVar(frame3)
 label6_text.set("")
 
-label2 = ttk.Label(frame2, text="使用する重り")
-label3 = ttk.Label(frame2, text="kg")
-label6 = ttk.Label(frame2, textvariable=label6_text)
-entry1 = ttk.Entry(frame2)
-button1 = ttk.Button(frame2, textvariable=button1_text, command=button1_click)
+label1 = ttk.Label(frame1, text="接続先")
+label2 = ttk.Label(frame3, text="使用する重り")
+label3 = ttk.Label(frame3, text="kg")
+label4 = ttk.Label(frame5, text="補正値")
+label5 = ttk.Label(frame5, textvariable=label5_text)
+label6 = ttk.Label(frame3, textvariable=label6_text)
+label7 = ttk.Label(frame2, text="ゼロ点調整")
 
-# frame3
-button3 = ttk.Button(frame3, text="選択したデータの削除", command=button3_click)
+entry2_text = tk.StringVar(frame5)
+
+entry1 = ttk.Entry(frame3)
+entry2 = ttk.Entry(frame5, textvariable=entry2_text)
+
+button1_text = tk.StringVar(frame3)
+button1_text.set("測定開始")
+button2_text = tk.StringVar(frame5)
+button2_text.set("送信")
+button6_text = tk.StringVar(frame5)
+button6_text.set("開始")
+
+button1 = ttk.Button(frame3, textvariable=button1_text, command=button1_click)
+button2 = ttk.Button(frame5, textvariable=button2_text, command=button2_click)
+button3 = ttk.Button(frame4, text="選択したデータの削除", command=button3_click)
+button4 = ttk.Button(frame5, text="更新", command=button4_click)
+button5 = ttk.Button(frame5, text="前回の\n補正値", command=button5_click)
+button6 = ttk.Button(frame2, textvariable=button6_text, command=button6_click)
+
+combobox1 = ttk.Combobox(frame1, width=40, state="readonly", values=[])
 
 # ツリービューの作成
-tree = ttk.Treeview(frame3)
+tree = ttk.Treeview(frame4)
 # 列インデックスの作成
 tree["columns"] = (1,2,3,4)
 # 表スタイルの設定(headingsはツリー形式ではない、通常の表形式)
@@ -191,19 +237,6 @@ tree.heading(4,text="判定")
 #             (行インデックス番号を指定することもできる)
 # 3番目の引数-values:レコードの値をタプルで指定する
 
-# frame4
-label5_text = tk.StringVar(frame4)
-label5_text.set("")
-button2_text = tk.StringVar(frame4)
-button2_text.set("送信")
-entry2_text = tk.StringVar(frame4)
-label4 = ttk.Label(frame4, text="補正値")
-label5 = ttk.Label(frame4, textvariable=label5_text)
-entry2 = ttk.Entry(frame4, textvariable=entry2_text)
-button2 = ttk.Button(frame4, textvariable=button2_text, command=button2_click)
-button4 = ttk.Button(frame4, text="更新", command=button4_click)
-button5 = ttk.Button(frame4, text="前回の\n補正値", command=button5_click)
-
 # ===============================================================
 # ビューの配置
 # farme1
@@ -211,17 +244,21 @@ label1.grid(row=0, column=0, padx=5, pady=5, ipadx=5, ipady=5)
 combobox1.grid(row=0, column=1, padx=5, pady=5, ipadx=5, ipady=5)
 
 # frame2
+label7.grid(row=0, column=0, padx=5, pady=5, ipadx=5, ipady=5)
+button6.grid(row=0, column=1, padx=5, pady=5, ipadx=5, ipady=5)
+
+# frame3
 label2.grid(row=0, column=0, padx=5, pady=5, ipadx=5, ipady=5)
 entry1.grid(row=0, column=1, padx=5, pady=5, ipadx=5, ipady=5)
 label3.grid(row=0, column=2, padx=0, pady=5, ipadx=5, ipady=5)
 button1.grid(row=1, column=1, padx=10, pady=5, ipadx=5, ipady=5)
 label6.grid(row=1, column=2, padx=10, pady=5, ipadx=5, ipady=5)
 
-# frame3
+# frame4
 tree.grid(row=0,column=0, padx=5, pady=5, ipadx=5, ipady=5)
 button3.grid(row=1,column=0, padx=5, pady=5, ipadx=5, ipady=5)
 
-#frame4
+#frame5
 label4.grid(row=0, column=0, padx=5, pady=5, ipadx=5, ipady=5)
 
 entry2.grid(row=0, column=1, padx=5, pady=5, ipadx=5, ipady=5)
